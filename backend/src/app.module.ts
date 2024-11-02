9bfb16f26d1e2db6bb1cd7d76d50879204b39cf7
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-02 22:06:12 +0500

    Импорт i18n

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'ru',
+      loaderOptions: {
+        path: join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [AcceptLanguageResolver],
+    }),
+  ],
 })
 export class AppModule {}
