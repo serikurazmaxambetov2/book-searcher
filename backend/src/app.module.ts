f5b4b1af30a89af9ecaa01840176d815c5031f6d
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-02 22:07:25 +0500

    Импорт book

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { BookModule } from './book/book.module';
 import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
 import { join } from 'path';
 
 @Module({
   imports: [
     PrismaModule,
+    BookModule,
     I18nModule.forRoot({
       fallbackLanguage: 'ru',
       loaderOptions: {
