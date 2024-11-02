8bfe5d13d7243e583f372b50adaa3d1f530d7d66
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-02 22:03:56 +0500

    Настройки для i18n

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new I18nValidationPipe({ whitelist: true, transform: true }),
+  );
+  app.useGlobalFilters(new I18nValidationExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
