fcc76e2f82bbbc31d6cc7344e27d8da23d66a1a9
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-03 19:01:16 +0500

    Подключение базового диалога

--- bot/bot/handlers/start.py
+++ bot/bot/handlers/start.py
@@ -2,9 +2,11 @@ from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram_dialog import DialogManager
 
+from ..states.main import MainSG
+
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(_: types.Message, dialog_manager: DialogManager):
-    pass
+    await dialog_manager.start(state=MainSG.MAIN)
