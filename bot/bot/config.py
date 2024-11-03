76d170a8c958d405287a3beb5e6012a9e6ca2e5c
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-03 19:24:33 +0500

    Добавлен поле BACKEND_URL в config

--- bot/bot/config.py
+++ bot/bot/config.py
@@ -6,3 +6,6 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 # Настройки FSM
 REDIS_HOST = getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(getenv("REDIS_PORT", "6379"))
+
+# Настройки backend
+BACKEND_URL = getenv("BACKEND_URL", "http://localhost:3000")
