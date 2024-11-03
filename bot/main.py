cdc762d3be1be32fedf028cef8df84dfc13f9d57
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-03 18:47:53 +0500

    Добавлено логирование

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,14 @@
 import asyncio
+import logging
 
 from bot.misc import bot, dp
 
 
 async def main():
+    # Подключаем логирование
+    logging.basicConfig(level=logging.INFO)
+
+    # Запуск
     await dp.start_polling(bot)
 
 
