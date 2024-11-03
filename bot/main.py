a30080ce901440d799bed3c7743bbb7deb188a05
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-03 18:49:59 +0500

    Подключение обработчиков

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,16 @@
 import asyncio
 import logging
 
+from bot.handlers import router
 from bot.misc import bot, dp
 
 
 async def main():
     # Подключаем логирование
     logging.basicConfig(level=logging.INFO)
+
+    # Подключаем обработчики
+    dp.include_router(router)
 
     # Запуск
     await dp.start_polling(bot)
