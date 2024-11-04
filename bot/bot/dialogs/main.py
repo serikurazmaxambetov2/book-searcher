de2bd1391dd0ff2d8cdfe287a77142708dbe218d
Serik Urazmaxambetov <serikurazmaxambetov.v2@gmail.com>
2024-11-04 12:33:31 +0500

    Добавление логики в поисковик

--- bot/bot/dialogs/main.py
+++ bot/bot/dialogs/main.py
@@ -1,8 +1,12 @@
-from aiogram.types import Message
-from aiogram_dialog import Dialog, DialogManager, Window
-from aiogram_dialog.widgets.input import ManagedTextInput, TextInput
-from aiogram_dialog.widgets.text import Const
+import math
 
+from aiogram.types import Message
+from aiogram_dialog import Dialog, DialogManager, ShowMode, Window
+from aiogram_dialog.widgets.input import ManagedTextInput, TextInput
+from aiogram_dialog.widgets.kbd import Back, Button, Row
+from aiogram_dialog.widgets.text import Case, Const, Format, List, Multi
+
+from ..keyboards import next_page_button, prev_page_button
 from ..services import api_service
 from ..states.main import MainSG
 
@@ -10,16 +14,27 @@ from ..states.main import MainSG
 async def on_success(
     message: Message, widget: ManagedTextInput, dialog_manager: DialogManager, data: str
 ):
-    # Обработка...
+    await message.delete()
+
+    # Получаем данные с backend
     response = await api_service.search(data)
     total = response.get("total")
-    items = response.get("data")
+    items = [(index, item) for index, item in enumerate(response.get("data"), start=1)]
 
-    await dialog_manager.update(
-        {"total": total, "items": items, "current_page": 1, "max_page": total // 10 + 1}
-    )
-    await dialog_manager.switch_to(MainSG.SEARCH)
-    # ...
+    # Обновляем состояние
+    dialog_manager.dialog_data["text"] = data
+    dialog_manager.dialog_data["total"] = total
+    dialog_manager.dialog_data["items"] = items
+    dialog_manager.dialog_data["current_page"] = 1
+    dialog_manager.dialog_data["max_page"] = math.ceil(total / 10)
+    dialog_manager.dialog_data["is_not_empty"] = total != 0
+
+    # Переходим к следующему состоянию
+    await dialog_manager.next(ShowMode.EDIT)
+
+
+async def getter(dialog_manager: DialogManager, **_):
+    return dialog_manager.dialog_data
 
 
 main_dialog = Dialog(
@@ -27,5 +42,30 @@ main_dialog = Dialog(
         Const("Введите слово для поиска:"),
         TextInput(on_success=on_success, id="search_input"),
         state=MainSG.MAIN,
-    )
+    ),
+    Window(
+        Case(
+            {
+                False: Const("Совпадений нет"),
+                True: Multi(
+                    Const("Вот совпадения:"),
+                    List(
+                        Format("{item[0]}. {item[1][title]}"),
+                        items="items",
+                    ),
+                    sep="\n\n",
+                ),
+            },
+            selector="is_not_empty",
+        ),
+        Row(
+            prev_page_button,
+            Button(Format("{current_page} | {max_page}"), id="current_page"),
+            next_page_button,
+            when="is_not_empty",
+        ),
+        Back(Const("Назад")),
+        getter=getter,
+        state=MainSG.SEARCH,
+    ),
 )
